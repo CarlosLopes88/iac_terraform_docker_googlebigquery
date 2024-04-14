7cb1431f6085751f34ea57d0bd2a0396c608354b
Carlos Eduardo Lopes <89598254+CarlosLopes88@users.noreply.github.com>
2024-04-14 20:46:33 -0300

    Update ingestao.py

--- ingestao.py
+++ ingestao.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google.cloud import storage
 import requests
+from google.cloud.exceptions import Conflict, NotFound
 
 # Carregar as variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -12,11 +13,25 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CRE
 # Crie um cliente do Google Cloud Storage
 client = storage.Client()
 
+def obter_bucket(nome_bucket):
+    """Obtém um bucket existente do Google Cloud Storage"""
+    try:
+        bucket = client.get_bucket(nome_bucket)
+        print(f"Bucket '{bucket.name}' encontrado.")
+        return bucket
+    except NotFound:
+        print(f"Bucket '{nome_bucket}' não encontrado.")
+        raise
+
 def criar_bucket(nome_bucket):
-    """Cria um bucket no Google Cloud Storage"""
-    bucket = client.create_bucket(nome_bucket)
-    print(f"Bucket '{bucket.name}' criado com sucesso.")
-    return bucket
+    """Cria um bucket no Google Cloud Storage, se ainda não existir"""
+    try:
+        bucket = client.create_bucket(nome_bucket)
+        print(f"Bucket '{bucket.name}' criado com sucesso.")
+        return bucket
+    except Conflict:
+        print(f"Bucket '{nome_bucket}' já existe. Não é necessário criar um novo.")
+        return obter_bucket(nome_bucket)
 
 def enviar_arquivo_para_bucket(bucket, url, nome_objeto):
     """Envia um arquivo de uma URL para um bucket do Google Cloud Storage"""
@@ -31,10 +46,13 @@ url_arquivo1 = os.getenv("URL_ARQUIVO_1")
 url_arquivo2 = os.getenv("URL_ARQUIVO_2")
 url_arquivo3 = os.getenv("URL_ARQUIVO_3")
 
-# Crie o bucket no Google Cloud Storage
-bucket = criar_bucket(nome_bucket)
+# Tente obter ou criar o bucket
+try:
+    bucket = obter_bucket(nome_bucket)
+except NotFound:
+    bucket = criar_bucket(nome_bucket)
 
 # Envie os arquivos diretamente do GitHub para o bucket
-enviar_arquivo_para_bucket(bucket, url_arquivo1, "tb_customer.csv")
+enviar_arquivo_para_bucket(bucket, url_arquivo1, "tb_customers.csv")
 enviar_arquivo_para_bucket(bucket, url_arquivo2, "tb_product.csv")
-enviar_arquivo_para_bucket(bucket, url_arquivo3, "tb_sales.csv")
+enviar_arquivo_para_bucket(bucket, url_arquivo3, "tb_sale.csv")
